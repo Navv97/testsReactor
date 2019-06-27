75f6c983500b079f887cced3332b48a8dd248514
Grzegorz Kabziński <grzesiek.kab@gmail.com>
2019-06-27 15:29:07 +0200

    Add init, add test for door open

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -1,15 +1,40 @@
 package edu.iis.mto.testreactor.exc4;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class DishWasherTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    @Mock
+    private DirtFilter dirtFilter;
+    @Mock
+    private Door door;
+    @Mock
+    private Engine engine;
+    @Mock
+    private WaterPump waterPump;
+
+    private DishWasher dishWasher;
+    private ProgramConfiguration programConfiguration;
+
+    @Before
+    public void init(){
+        dishWasher = new DishWasher(waterPump,engine,dirtFilter,door);
     }
+
+    @Test
+    public void testIfDishWasherDoorsAreOpen(){
+        programConfiguration = ProgramConfiguration.builder().withProgram(WashingProgram.ECO).withTabletsUsed(true).build();
+        when(door.closed()).thenReturn(true);
+        assertEquals(dishWasher.start(programConfiguration).getStatus(),Status.DOOR_OPEN_ERROR);
+    }
+
 
 }
